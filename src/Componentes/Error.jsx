63a51fad50d3fbd184cabf5cdd9005b811441c5e
PabloAtahoneroGdB <alumno.107253@iesazarquiel.es>
2024-02-23 14:05:53 +0100

    ReactRouter WIP

---
+++ src/Componentes/Error.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function Error() {
+    return (
+        <div>Esto ha sido un error</div>
+    )
+}
--- src/index.jsx
+++ src/index.jsx
@@ -1,12 +1,33 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+import ReactDOM from 'react-dom';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Principal from './Componentes/Principal';
+import Error from './Componentes/Error';
+import Search from './Componentes/Search';
+import Detalles from './Componentes/Detalles';
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Principal />,
+    errorElement: <Error />,
+    children: [{
+      path: "/searchByName",
+      element: <Search />,
+    },
+
+    ],
+  },
+  {
+    path: "/detalles",
+    element: <Detalles />
+  }
+]);
+
+ReactDOM.render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>,
+  document.getElementById('root')
 );
-
