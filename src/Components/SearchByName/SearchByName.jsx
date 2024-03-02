91f7053fbb1e76fc5b6774f81855c75d86c0c251
PabloAtahoneroGdB <alumno.107256@iesazarquiel.es>
2024-03-03 00:59:14 +0100

    React Icons added,better implementation and updates on SearchResults info and working on styles

--- src/Components/SearchByName/SearchByName.jsx
+++ src/Components/SearchByName/SearchByName.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import { CiSearch } from 'react-icons/ci';
 import { getMealsByName } from '../../services/searchByName';
 import MealList from '../MealList/MealList';
 import SearchResultsInfo from '../SearchResultsInfo/SearchResultsInfo';
@@ -7,6 +8,7 @@ export default function SearchByName() {
   const [formField, setFormField] = useState();
   const [listaResultante, setListaResultante] = useState([]);
   const [resultsCount, setResultsCount] = useState(0);
+  const [searchButtonClicked, setSearchButtonClicked] = useState(false);
 
   const countResults = (results) => {
     if (results) { setResultsCount(results.length); } else { setResultsCount(0); }
@@ -16,40 +18,59 @@ export default function SearchByName() {
     setFormField(event.target.value);
   };
   const searchMealsByName = () => {
+    // esto es para poner lo de la búsqueda a true
+
     getMealsByName(formField).then(
       (json) => {
         setListaResultante(json.meals);
         countResults(json.meals);
+        setSearchButtonClicked(true);
       },
     );
   };
 
   return (
     <>
-      <div>Pagina SearchByName</div>
-      <input
-        type="text"
-        name=""
-        id=""
-        placeholder="Search by Name"
-        onChange={formFieldHandler}
-      />
-      <button
-        type="button"
-        className="btn btn-primary"
-        onClick={searchMealsByName}
-      >
-        Button
-      </button>
-      {/* Esto es para que si existe algo más en la lista lo pinte */}
-      {resultsCount
-        ? (
-          <>
-            <SearchResultsInfo numResultados={resultsCount} />
-            <MealList mealsToPrint={listaResultante} />
-          </>
-        )
-        : <SearchResultsInfo numResultados={resultsCount} />}
+      <div>
+        <h1 className="text-center">Seach a meal by it&apos;s name</h1>
+      </div>
+      <div className="row w-75 mx-auto">
+        <input
+          type="text"
+          name=""
+          id=""
+          placeholder="Search by Name"
+          onChange={formFieldHandler}
+        />
+        <button
+          type="button"
+          className="btn btn-primary"
+          onClick={searchMealsByName}
+          aria-label="Search"
+        >
+          <p className="mb-0 fw-bold"><CiSearch /></p>
+
+        </button>
+      </div>
+      <div className="container">
+        {/* Esto es para que si existe algo más en la lista lo pinte */}
+        {resultsCount
+          ? (
+            <>
+              <SearchResultsInfo
+                numResultados={resultsCount}
+                searchButtonClicked={searchButtonClicked}
+              />
+              <MealList mealsToPrint={listaResultante} />
+            </>
+          )
+          : (
+            <SearchResultsInfo
+              numResultados={resultsCount}
+              searchButtonClicked={searchButtonClicked}
+            />
+          )}
+      </div>
     </>
   );
 }
