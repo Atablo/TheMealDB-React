5211e8ece5b45d5ca16b8bd1337f617194dfc800
YiYangJi <alumno.621144@ies-azarquiel.es>
2024-03-08 11:03:05 +0100

    SearchByIngredient on MealDetails added

---
+++ src/Components/IngredientPage/IngredientRecipesPage.jsx
@@ -0,0 +1,61 @@
+import React, { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
+import MealList from '../MealList/MealList';
+import {
+  getIngredientsByName,
+  getMealsByName,
+} from '../../services/searchByIngredient';
+
+function IngredientRecipesPage() {
+  const { ingredient } = useParams();
+  const [detailedRecipes, setDetailedRecipes] = useState([]);
+
+  useEffect(() => {
+    const fetchDetailedRecipes = async () => {
+      try {
+        const data = await getIngredientsByName(ingredient);
+
+        if (data.meals) {
+          const detailedRecipesData = await Promise.all(
+            data.meals.map(async (recipe) => {
+              try {
+                const detailData = await getMealsByName(recipe.strMeal);
+                return detailData.meals ? detailData.meals[0] : null;
+              } catch (error) {
+                console.error(
+                  `Error fetching details for meal ${recipe.strMeal}:`,
+                  error,
+                );
+                return null;
+              }
+            }),
+          );
+
+          const filteredDetailedRecipes = detailedRecipesData.filter(
+            (recipe) => recipe !== null,
+          );
+          setDetailedRecipes(filteredDetailedRecipes);
+        }
+      } catch (error) {
+        console.error('Error fetching related recipes:', error);
+      }
+    };
+
+    fetchDetailedRecipes();
+  }, [ingredient]);
+
+  return (
+    <div className="container">
+      <div className="row flex-wrap justify-content-center m-2 mt-4">
+        <h4 className="card-title mb-4 bg-info-subtle rounded-2 p-2 text-center">
+          Related Recipes for {ingredient}
+        </h4>
+        {detailedRecipes.length > 0 && (
+          <MealList mealsToPrint={detailedRecipes} />
+        )}
+      </div>
+    </div>
+  );
+}
+
+export default IngredientRecipesPage;
--- src/Components/MealDetails/MealDetails.jsx
+++ src/Components/MealDetails/MealDetails.jsx
@@ -1,17 +1,18 @@
 import React, { useEffect, useState } from 'react';
-import { useParams } from 'react-router-dom';
+import { useParams, useNavigate } from 'react-router-dom';
 import Error from '../../Componentes/Error';
+import MealList from '../MealList/MealList';
 
 function MealDetails() {
+  const navigate = useNavigate();
   const { id } = useParams();
   const [mealData, setMealData] = useState(null);
+  const [relatedRecipes, setRelatedRecipes] = useState([]);
 
   useEffect(() => {
     const fetchMealDetails = async () => {
       try {
-        const response = await fetch(
-          `https://www.themealdb.com/api/json/v1/1/lookup.php?i=${id}`,
-        );
+        const response = await fetch(`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${id}`);
         const data = await response.json();
         if (data.meals && data.meals.length > 0) {
           setMealData(data.meals[0]);
@@ -24,11 +25,49 @@ function MealDetails() {
     fetchMealDetails();
   }, [id]);
 
+  // Dentro de la función fetchRelatedRecipes
+  const fetchRelatedRecipes = async (ingredient) => {
+    try {
+      const response = await fetch(`https://www.themealdb.com/api/json/v1/1/filter.php?i=${ingredient}`);
+      const data = await response.json();
+
+      if (data.meals) {
+        const idMeals = data.meals.map((meal) => meal.idMeal);
+
+        const detailedRecipes = await Promise.all(idMeals.map(async (idMeal) => {
+          try {
+            const detailResponse = await fetch(`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${idMeal}`);
+            const detailData = await detailResponse.json();
+            return detailData.meals ? detailData.meals[0] : null;
+          } catch (error) {
+            console.error(`Error fetching details for meal ${idMeal}:`, error);
+            return null;
+          }
+        }));
+
+        const filteredDetailedRecipes = detailedRecipes.filter((recipe) => recipe !== null);
+
+        setRelatedRecipes(filteredDetailedRecipes);
+      }
+    } catch (error) {
+      console.error('Error fetching related recipes:', error);
+    }
+  };
+
+  const handleIngredientClick = (ingredient) => {
+    navigate(`/ingredient-recipes/${ingredient}`);
+  };
+
+  const handleIngredientKeyDown = (event, ingredient) => {
+    if (event.key === 'Enter') {
+      fetchRelatedRecipes(ingredient);
+    }
+  };
+
   if (!mealData) {
     return <Error />;
   }
 
-  // Extract ingredients and measures
   const ingredients = [];
   for (let i = 1; i <= 20; i += 1) {
     const ingredient = mealData[`strIngredient${i}`];
@@ -39,6 +78,7 @@ function MealDetails() {
   }
 
   const instructionsArray = mealData.strInstructions.split('. ');
+
   return (
     <div className="container">
       <div className="row mt-5">
@@ -58,15 +98,11 @@ function MealDetails() {
                     <h2 className="card-title fw-bold">{mealData.strMeal}</h2>
                     <div className="d-flex justify-content-between shadow p-3 rounded-5 mt-3">
                       <div className="bg-warning w-50 d-flex justify-content-center rounded-4 align-items-center">
-                        <strong className="align-items-center type">
-                          {mealData.strCategory}
-                        </strong>
+                        <strong className="align-items-center type">{mealData.strCategory}</strong>
                       </div>
                       <div className="w-50 d-flex justify-content-center align-items-center">
                         <div className="w-50 d-flex justify-content-center align-items-center d-none d-md-flex">
-                          <strong className="fs-6 country">
-                            {mealData.strArea}
-                          </strong>
+                          <strong className="fs-6 country">{mealData.strArea}</strong>
                         </div>
                         <img
                           src={`https://www.themealdb.com/images/icons/flags/big/32/${mealData.strArea}.png`}
@@ -76,13 +112,16 @@ function MealDetails() {
                       </div>
                     </div>
                     <div className="row flex-wrap justify-content-center shadow rounded-3 m-2 mt-4">
-                      <h4 className="card-title mb-4 bg-info-subtle rounded-2 p-2 text-center">
-                        Ingredients
-                      </h4>
-                      {ingredients.map((ingredient) => (
+                      <h4 className="card-title mb-4 bg-info-subtle rounded-2 p-2 text-center">Ingredients</h4>
+                      {ingredients.map((ingredient, index) => (
                         <div
                           key={`${ingredient.ingredient}-${ingredient.measure}`}
                           className="ingredient col-1 col-sm-1 col-lg-2 col-xl-2 my-2 text-center ingredient-container"
+                          onClick={() => handleIngredientClick(ingredient.ingredient)}
+                          onKeyDown={(e) => handleIngredientKeyDown(e, ingredient.ingredient)}
+                          role="button"
+                          tabIndex={0}
+                          style={{ cursor: 'pointer' }}
                         >
                           <div className="bg-cardbody rounded-4 p-2">
                             <img
@@ -91,18 +130,14 @@ function MealDetails() {
                               className="card-img-top img-fluid"
                             />
                           </div>
-                          <p>{`${ingredient.measure} ${ingredient.ingredient}`}</p>
+                          <p className={`ingredient${index + 1} m-0`}>{`${ingredient.measure} ${ingredient.ingredient}`}</p>
                         </div>
                       ))}
+
                     </div>
                   </div>
                   <div className="text-end mt-2 mb-3">
-                    <a
-                      href={mealData.strSource}
-                      className="text-decoration-none pe-4"
-                      target="_blank"
-                      rel="noreferrer"
-                    >
+                    <a href={mealData.strSource} className="text-decoration-none pe-4" target="_blank" rel="noreferrer">
                       More information...
                     </a>
                   </div>
@@ -123,16 +158,19 @@ function MealDetails() {
           ))}
           <div className="w-75 mx-auto mt-4">
             <div className="embed-responsive ratio ratio-16x9 mb-3">
-              <iframe
-                className="embed-responsive-item"
-                src={`https://www.youtube.com/embed/${mealData.strYoutube.split('v=')[1]}`}
-                title="Meal Video"
-                allowFullScreen
-              />
+              <iframe className="embed-responsive-item" src={`https://www.youtube.com/embed/${mealData.strYoutube.split('v=')[1]}`} title="Meal Video" allowFullScreen />
             </div>
           </div>
         </div>
       </div>
+
+      {/* Display related recipes */}
+      {relatedRecipes.length > 0 && (
+        <div className="row flex-wrap justify-content-center shadow rounded-3 m-2 mt-4">
+          <h4 className="card-title mb-4 bg-info-subtle rounded-2 p-2 text-center">Related Recipes</h4>
+          <MealList mealsToPrint={relatedRecipes} />
+        </div>
+      )}
     </div>
   );
 }
