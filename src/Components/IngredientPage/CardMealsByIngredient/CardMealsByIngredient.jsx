3217a097fc4758c7f8c5619fffcdec8a825ded76
johnnyji6 <johnnyji4091@gmail.com>
2024-03-09 22:24:05 +0100

    Co-authored-by: Pablo Atahonero García de Blas <Atablo@users.noreply.github.com>

--- src/Components/IngredientPage/CardMealsByIngredient/CardMealsByIngredient.jsx
+++ src/Components/IngredientPage/CardMealsByIngredient/CardMealsByIngredient.jsx
@@ -15,46 +15,68 @@ export default function CardMealsByIngredient({ nameIngredient }) {
 
   // Creamos otro useState que almacenará lo mismo que mealList pero como copia
   const [listaFiltrada, setListaFiltrada] = useState([]);
+  // Creamos un useState para contar el numero de resultados
   const [resultsCount, setResultsCount] = useState(0);
+  // Creamos un useState para comprobar si se ha clickado el botón de búsqueda
   const [searchButtonClicked, setSearchButtonClicked] = useState(false);
 
+  // Creo una función que le paso una comida
   const onMealList = (plato) => {
+    // Hacemos que el mealList contenga los meals desestructurados, mas la comida actual
     setMealList((currentMeals) => [...currentMeals, plato.meals[0]]);
+    // Repetimos el proceso con la listaFiltrada
     setListaFiltrada((currentMeals) => {
+      // Creamos una variable que almacene los meals desestructurados, mas la comida actual
       const newListaFiltrada = [...currentMeals, plato.meals[0]];
+      // Hacemos que el resultsCount sea igual al numero de comidas que haya
       setResultsCount(newListaFiltrada.length);
+      // Devolvemos la nueva listaFiltrada para que sea listaFiltrada
       return newListaFiltrada;
     });
   };
 
+  // Hacemos un useEffect que se ejecute cuando cambie el nombre del ingrediente
   useEffect(() => {
+    // Si el nombre del ingrediente introducido no está vacio
     if (nameIngredient) {
+      // Vaciamos el mealList
       setMealList([]);
+      // Vaciamos la listaFiltrada
       setListaFiltrada([]);
+
+      // Obtenemos todos las comidas con ese ingrediente
       getIngredientsByName(nameIngredient).then((meals) => {
+        // Si el array de meals no está vacio:
         if (meals.meals) {
           // Por cada array meals del objeto meals:
           meals.meals.forEach((meal) => {
+            // Obtenemos todos las comidas con datos de la comida concreta
             getMealsByName(meal.strMeal).then((plato) => {
+              // LLamamos a la funcion onMealList pasandole la comida
               onMealList(plato);
+              // Cambiamos searchButtonClicked a true
               setSearchButtonClicked(true);
             });
           });
+          // Si el array de meals se encuentra vacio:
         } else {
+          // Cambiamos searchResultsCount a 0
           setResultsCount(0);
+          // Y searchButtonClicked a true
           setSearchButtonClicked(true);
         }
       });
     }
   }, [nameIngredient]);
 
+  // Creamos una funcion para aplicar filtros que recibe la categoriaSeleccionada,
+  // regionSeleccionada y etiquetaSelccionada
   const applyFilters = (
     categoriaSeleccionada,
     regionSeleccionada,
     etiquetaSelccionada,
   ) => {
     // hacemos una copia del array original y trabajamos con ella
-    // setListaFiltrada([...listaOriginal]);
     let listaAux = [...mealList];
     /*  Aquí haremos las comprobaciones */
     if (categoriaSeleccionada !== '--') {
@@ -67,9 +89,10 @@ export default function CardMealsByIngredient({ nameIngredient }) {
     }
     if (etiquetaSelccionada !== '--') {
       /* Para las etiqueta será distinto,pues hay varias etiquetas que puede tener una comida */
-      // eslint-disable-next-line max-len
       listaAux = listaAux.filter((item) => {
+        // En caso de que tenga tags
         if (item.strTags) {
+          // Devolvemos los tags pasados en mayusculas y que coincidan con la seleccionada
           return item.strTags
             .toUpperCase()
             .includes(etiquetaSelccionada.toUpperCase());
@@ -77,19 +100,22 @@ export default function CardMealsByIngredient({ nameIngredient }) {
         return false; // Devolver false si item.strTags no existe o es null
       });
     }
-    /** Aquí hay duda!! como puedo volver a almacenar el array orignal??? */
-    setListaFiltrada(listaAux);
-
+    // Una vez hayamos hecho todas las comprobaciones con los filtros lo igualamos a listaFiltrada,
     // una vez acabadas las comprobaciones insertaremos ese array
+    setListaFiltrada(listaAux);
   };
 
+  // Creamos una variable que reinicie los filtros pasandole los valores
   const resetFilters = (
     setCategoriaSeleccionada,
     setRegionSeleccionada,
     setEtiquetaSeleccionada,
   ) => {
+    // Igualamos la listaFiltrada a la lista original
     setListaFiltrada(mealList);
+    // Igualamos el conteo a la longitud de los elementos de la lista original
     setResultsCount(mealList.length);
+    // Reseteamos todos los filtros
     setCategoriaSeleccionada('--');
     setRegionSeleccionada('--');
     setEtiquetaSeleccionada('--');
@@ -97,28 +123,38 @@ export default function CardMealsByIngredient({ nameIngredient }) {
 
   return (
     <div>
+      {/* Operador ternario que si se cumple que si el nombre
+      de ingrediente no está vacio y el array tiene elementos: */}
       {nameIngredient && listaFiltrada.length > 0 ? (
         <>
           <div className="container">
+            {/* Incluimos el componente FiltersBox */}
             <FiltersBox
-              // le voy a pasar la lisa original,
-              // el seteador(para que imprima correctamente)
-              // y además el metodo para cuando lo reseteen
+              // Le pasamos el applyFilters
+              // y el resetFilters
               applyFilters={applyFilters}
               resetFilters={resetFilters}
               className="w-75"
             />
+            {/* Incluimos el componente SearchResultsInfo */}
             <SearchResultsInfo
+              // Le pasamos el contador de resultados
               numResultados={resultsCount}
+              // y la variable de si el boton está clickado
               searchButtonClicked={searchButtonClicked}
             />
           </div>
+          {/* Incluimos el componente Mealist pasandole la lista de comidas */}
           <MealList mealsToPrint={listaFiltrada} />
         </>
       ) : (
+        // En caso de que no se cumpla:
         <div className="container">
+          {/* Incluimos el componente SearchResultsInfo */}
           <SearchResultsInfo
+            // Le pasamos el contador de resultados
             numResultados={resultsCount}
+            // y la variable de si el boton está clickado
             searchButtonClicked={searchButtonClicked}
           />
         </div>
@@ -127,10 +163,12 @@ export default function CardMealsByIngredient({ nameIngredient }) {
   );
 }
 
+// Ponemos los propTypes indicando que el parametro nameIngredient
 CardMealsByIngredient.propTypes = {
   nameIngredient: PropTypes.string,
 };
 
+// Y también ponemos los propTypes por defecto
 CardMealsByIngredient.defaultProps = {
   nameIngredient: '',
 };
--- src/Components/IngredientPage/IngredientRecipesPage/IngredientRecipesPage.jsx
+++ src/Components/IngredientPage/IngredientRecipesPage/IngredientRecipesPage.jsx
@@ -3,29 +3,46 @@ import { useParams } from 'react-router-dom';
 import MealList from '../../MealList/MealList';
 import { getIngredientsByName, getMealsByName } from '../../../services/searchByIngredient';
 
+// Componente encargado de mostrar la página de las comidas que contienen un ingrediente en concreto
 function IngredientRecipesPage() {
+  // Creamos una variable que extrae el parametro del ingrediente de la URL
   const { ingredient } = useParams();
+  // Creamos un useState que almacene una lista de comidas del ingrediente
   const [detailedRecipes, setDetailedRecipes] = useState([]);
 
+  // Creamos un useState
   useEffect(() => {
+    // Creamos una función asíncrona
     const fetchDetailedRecipes = async () => {
       try {
+        // Creamos una variable que almacene la lista de comidas que contenga ese ingrediente
         const data = await getIngredientsByName(ingredient);
 
+        // Si la lista no se encuentra vacia:
         if (data.meals) {
+          // Creamos una variable que espere a que se almacenen todas las promesas
           const detailedRecipesData = await Promise.all(
+            // Mapeamos la lista de comidas
             data.meals.map(async (recipe) => {
               try {
+                // Para cada receta, obtenemos la comida con detalles
+                // más específicos por el nombre de la comida
                 const detailData = await getMealsByName(recipe.strMeal);
+                // Si la lista de meals no está vacia,
+                // devolvemos la comida de la lista; de lo contrario, null.
                 return detailData.meals ? detailData.meals[0] : null;
               } catch (detailError) {
+                // En caso de error al buscar los detalles de una
+                // receta, se muestra el error al usuario
                 console.error(`Error fetching details for recipe ${recipe.strMeal}:`, detailError);
                 return null;
               }
             }),
           );
 
+          // Creamos una variable para filtrar las recetas y eliminar aquellas que no tengan comidas
           const filteredDetailedRecipes = detailedRecipesData.filter((recipe) => recipe !== null);
+          // Hacemos que detailedRecipes sea igual a filteredDetailedRecipes
           setDetailedRecipes(filteredDetailedRecipes);
         }
       } catch (fetchError) {
@@ -33,6 +50,7 @@ function IngredientRecipesPage() {
       }
     };
 
+    // Ejecutamos la función
     fetchDetailedRecipes();
   }, [ingredient]);
 
@@ -40,8 +58,11 @@ function IngredientRecipesPage() {
     <div className="container">
       <div className="row flex-wrap justify-content-center m-2 mt-4">
         <h4 className="card-title mb-4 bg-info-subtle rounded-2 p-2 text-center">
+          {/* Mostramos el texto con el ingrediente de la URL */}
           Related Recipes for {ingredient}
         </h4>
+        {/* Si la lista de comidas fltrada no está vacia incluimos
+        el componente MealList pasandole la lista de comidas */}
         {detailedRecipes.length > 0 && <MealList mealsToPrint={detailedRecipes} />}
       </div>
     </div>
