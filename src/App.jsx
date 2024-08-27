b62c4d7e7303a36d727eca099136e43026887c0e
PabloAtahoneroGdB <alumno.107256@iesazarquiel.es>
2024-08-27 12:53:36 +0200

    Forth try to publish page

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,9 @@
 import React from 'react';
-import { Route, Routes, Navigate } from 'react-router-dom';
+import {
+  Route,
+  Routes,
+  Navigate,
+} from 'react-router-dom'; // No necesitas importar HashRouter aquí
 import Error from './Components/Error/Error';
 import Home from './Components/Home/Home';
 import Header from './Components/Header/Header';
@@ -17,30 +21,22 @@ function App() {
         <Header />
       </header>
       <Routes>
-        <Route path="/" render={() => <Navigate to="/home" />} />
-        {/* La ruta raiz será el componente Home */}
+        {/* Redirige desde la raíz a la ruta /home */}
+        <Route path="/" element={<Navigate to="/home" />} />
+        {/* La ruta raíz será el componente Home */}
         <Route path="/home" element={<Home />} />
         {/* La ruta /searchByName incluirá el componente SearchByName */}
         <Route path="/searchByName" element={<SearchByName />} />
         {/* La ruta /searchByIngredient incluirá el componente SearchByIngredient */}
         <Route path="/searchByIngredient" element={<SearchByIngredient />} />
-        {/* Esta ruta /meal/id incluirá el componente MealDetails con su id corresponiendte */}
-        <Route
-          path="/meal/:id"
-          element={<MealDetails />}
-        />
-        {/* La ruta /ingredient-recipes/ingredient incluirá el componente
+        {/* Esta ruta /meal/:id incluirá el componente MealDetails con su id correspondiente */}
+        <Route path="/meal/:id" element={<MealDetails />} />
+        {/* La ruta /ingredient-recipes/:ingredient incluirá el componente
         IngredientRecipesPage con el resultado de las comidas que contienen ese ingrediente */}
-        <Route
-          path="/ingredient-recipes/:ingredient"
-          element={<IngredientRecipesPage />}
-        />
+        <Route path="/ingredient-recipes/:ingredient" element={<IngredientRecipesPage />} />
         {/* Cualquier ruta introducida que no sea alguna de las de arriba
         informará al usuario de que la página no se ha encontrado */}
-        <Route
-          path="*"
-          element={<Error />}
-        />
+        <Route path="*" element={<Error />} />
       </Routes>
     </div>
   );
--- src/index.jsx
+++ src/index.jsx
@@ -1,17 +1,14 @@
-import { StrictMode, React } from 'react';
+import React, { StrictMode } from 'react';
+
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  // Ponemos el StrictMode para resaltar y prevenir posibles problemas en la aplicación
   <StrictMode>
-    {/* Envolvemos toda la aplicacion con browserRouter para que funcionen las rutas */}
     <BrowserRouter>
-      {/* Incluimos el componente principal en la página */}
       <App />
     </BrowserRouter>
   </StrictMode>,
-
 );
